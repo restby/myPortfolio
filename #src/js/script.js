5bfb070079bb9e962e86d150a72e687573c0fc8f
restby <restby@gmail.com>
2020-07-25 00:05:50 +0300

    skills

--- #src/js/script.js
+++ #src/js/script.js
@@ -5,8 +5,18 @@ $(document).ready(function () {
   /*burger & navigation*/
   $('.main-header__burger').click(function (event) {
     event.preventDefault();
-    $('.main-header__burger,.main-header,.main-header__navigation').toggleClass('active');
+    $('.main-header__burger,.main-header__navigation').toggleClass('active');
     $('body').toggleClass('lock');
+  });
+  $('.navigation__link').click(function () {
+    $('.main-header__burger,.main-header__navigation').toggleClass('active');
+    $('body').toggleClass('lock');
+    let scroll_el = $(this).attr('href');
+    let destination = $(scroll_el).offset().top;
+    if ($(scroll_el).length != 0) {
+      $('html, body').animate({ scrollTop: destination - 79 }, 800);
+    }
+    return false;
   });
 });
 
